a184a479329eb6853d297325f2776479e365c73b
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-07-16 20:19:51 +0900

    Spring 게시판 연습
    화면이 안나오는 오류 픽스중....

---
+++ 자바/board_test-1/src/main/java/com/board/test/BoardTest1Application.java
@@ -0,0 +1,13 @@
+package com.board.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardTest1Application.class, args);
+	}
+
+}
