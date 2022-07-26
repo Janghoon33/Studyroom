1cf9493c0cf7147655a3e7d10f92a5f070ee47a2
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-07-26 23:39:53 +0900

    리액트 공부 3일차 (코딩앙마)

---
+++ 리액트/reactstudy/reactstudy/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  const name = "Tom";
+  const naver = {
+    name : "네이버",
+    url : "https://naver.com",
+  };
+  return (
+    <div className="App">
+      <h1 style={{
+        color : "#f0f",
+        backgroundColor : "green"
+      }}> 
+      Hello, {name}.<p>{2+3}</p></h1>
+      <a href={naver.url}>{naver.name}</a>
+    </div>
+  );
+}
+
+export default App;
