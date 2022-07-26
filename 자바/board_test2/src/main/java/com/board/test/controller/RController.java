604b6654281acfa72b5fd82bd11d2f92e76173f0
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-07-26 23:17:01 +0900

    리액트 연동 스프링부트 코드 (상세글보기 연동중)

--- 자바/board_test2/src/main/java/com/board/test/controller/RController.java
+++ 자바/board_test2/src/main/java/com/board/test/controller/RController.java
@@ -3,32 +3,45 @@ package com.board.test.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.board.test.domain.Board;
+import com.board.test.mapper.BoardMapper;
 import com.board.test.service.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class RController {
+	
+	private final Service service;
+	
 	@Autowired
-	private Service service;
+	BoardMapper mapper;
+
 	
-	@RequestMapping(value = "/board", method = {RequestMethod.GET})
-	public List<Board> userDataFindAll() {
-		List<Board> list = service.findAll();
-		return list;
+	@PostMapping("/api/board")
+	public List<Board> board() {
+		System.out.println("게시글 목록 페이지");
+	//		Board board = new Board(1, "jj","gg","hh","2022.07.23");
+		List<Board> board = mapper.boardList();
+		return  board;
 	}
 	
-	
-	@RequestMapping(value = "/boarda", method = {RequestMethod.GET})
-	public List<Board> boardList(Board board){
-		List<Board> list = service.boardList(board);
-		return list;
+	@PostMapping("/api/boardInsert")
+	public int boardInsert(@RequestBody Board board) {
+		System.out.println("게시글 작성완료");
+		return service.insertBoard(board);
 	}
 	
-	
-	
-	
+	@PostMapping("/api/boardContent/{b_num}")
+	public Board boardContent(@RequestBody int b_num) {
+		System.out.println("상세 글 보기"+b_num);
+		return service.boardContent(b_num);
+	}
 }
--- 자바/board_test2/src/main/java/com/board/test/service/Service.java
+++ 자바/board_test2/src/main/java/com/board/test/service/Service.java
@@ -1,34 +1,23 @@
 package com.board.test.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.board.test.domain.Board;
 import com.board.test.mapper.BoardMapper;
-import com.board.test.repo.Repo;
 
 @org.springframework.stereotype.Service
 public class Service {
 	
 	@Autowired
-	Repo repo;
-	
-	@Autowired
 	BoardMapper mapper;
 	
-	public List<Board> findAll(){
-		return repo.findAll();
+	
+	public int insertBoard(Board board) {
+		return mapper.insertBoard(board);
 	}
 	
-	
-	public List<Board> boardList(Board board){
-		return boardList(board);
+	public Board  boardContent(int b_num) {
+		return mapper.boardContent(b_num);
 	}
-
-	public int writeAction(String b_title, String b_content) {
-	return mapper.writeAction(b_title, b_content);
-	}	
 	
-
 }
