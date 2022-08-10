2b43b7866e267cc7a3bdfd80765ca34e238445e4
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-08-10 17:36:36 +0900

    스프링부트 공부 (블로그 만들어보기 by.메타코딩) 스프링부트 환경설정 및 실행

---
+++ 자바/스프링부트 공부(블로그 만들어보기)/hello/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+// 이클립스 = Ctrl + F11
+// KeyMap = 인텔리 J =  shift + f10
+public class HelloWorld {
+	// 스프링부트 연습을 위한 첫번째 테스트
+	public static void main(String[] args) {
+		System.out.println("헬러우 월드");
+		System.out.println("헬러우 월드");
+		System.out.println("헬러우 월드");
+		System.out.println("헬러우 월드");
+		System.out.println("헬러우 월드");
+		System.out.println("헬러우 월드");
+
+	}
+
+}
