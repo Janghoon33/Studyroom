91f1e988aee4f1387a0df09e56cffbd8205db067
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-08-12 14:43:32 +0900

    리액트 공부 5일차(코딩앙마) 이벤트 처리

--- 리액트/reactstudy/reactstudy/src/App.js
+++ 리액트/reactstudy/reactstudy/src/App.js
@@ -4,11 +4,10 @@ import Welcome from './component/Welcome';
 import styles from "./App.module.css";
 
 function App() {
-  
   return (
     <div className="App">
       <Hello/>
-      <div className={styles.box}>App</div>
+      
     </div>
   );
 }
--- 리액트/reactstudy/reactstudy/src/component/Hello.js
+++ 리액트/reactstudy/reactstudy/src/component/Hello.js
@@ -7,20 +7,41 @@ import styles from "./Hello.module.css";
 
 
 export default function Hello(){
+
+    function showName(){
+        console.log("Mike");
+      }
+      
+      function showAge(age){
+        console.log(age);
+      }
+
+    //   function showText(text){
+    //     console.log(e.target.value)
+    //   }
+    //   타겟 : input 태그  value : 작성한값
+    // return(<input type="text" onChange={(e)=>console.log(e.target.value)}/>)
+    // 2 번째 방법
+        function showText(txt){
+            console.log(txt);
+        }
    return (
     // jsx 는 하나의 태그만 사용가능! div나 <></>(빈태그) 태그로 전체를 묶어주기
    <div>  
-    <h1 style={
-        {
-            color : "#f00",
-            borderRight : '2px solid #000',
-            marginBottom : '30px',
-            opacity : 1,
-        }
-    }>
-        Hello
-        </h1>
-        <div className={styles.box}>Hello</div>
+    <h1>Hello</h1>
+    <button onClick={showName}>Show name</button> 
+    {/* 이벤트 적용 첫번째 방법 */}
+
+    <button onClick={()=>
+    {
+        showAge(30)
+    }}>Show age</button>   
+     {/* 이벤트 적용 두번째 방법 : 매개변수를 전달하기 편함*/}
+    <input type="text" onChange={e => {
+        const txt = e.target.value;
+        showText(txt);
+    }}/>  
+    
    </div>
    );
 }
