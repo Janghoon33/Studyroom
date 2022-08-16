261e4ea730e02bb7f986a13de113e1065830b251
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-08-16 10:37:52 +0900

    리액트 공부 6일차(코딩앙마) props

--- 리액트/reactstudy/reactstudy/src/App.js
+++ 리액트/reactstudy/reactstudy/src/App.js
@@ -7,9 +7,10 @@ function App() {
   
   return (
     <div className="App">
-      <Hello/>
-      <Hello/>
-      <Hello/>
+      <h3>props : properties</h3>
+      <Hello age={10}/>
+      <Hello age={20}/>
+      <Hello age={30}/>
     </div>
   );
 }
--- 리액트/reactstudy/reactstudy/src/component/Hello.js
+++ 리액트/reactstudy/reactstudy/src/component/Hello.js
@@ -1,23 +1,26 @@
 import World from "./World";
 import styles from "./Hello.module.css";
 import { useState } from "react";
+import UserName from "./UserName";
 // const Hello = () => {
 //     <p>Hello</p>
 // }
 // export default Hello;    <- 이렇게 써도 되고
 
 
-export default function Hello(){
+export default function Hello({ age }){
     // let name = "Mike";
     const [name, setName] = useState('Mike');
+    const msg = age > 19 ? "성인 입니다." : "미성년자 입니다.";
 
    return (
     // jsx 는 하나의 태그만 사용가능! div나 <></>(빈태그) 태그로 전체를 묶어주기
    <div>  
-        <h2 id="name">{name}</h2>
-        <button onClick={()=>(
-             setName(name === "Mike" ? "Jane" : "Mike")
-        )}>Change</button>
+        <h2 id="name">{name}({age}):{msg}</h2>
+        <UserName name={name}/>
+        <button onClick={()=>{
+             setName(name === "Mike" ? "Jane" : "Mike");
+        }}>Change</button>
    </div>
    );
 }
