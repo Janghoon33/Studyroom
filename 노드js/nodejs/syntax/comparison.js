296120e4d47618df57c38f6446f932022828db81
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-08-22 14:39:49 +0900

    Node.js 공부 2일차 Boolean, 비교연산자, 제어문, 조건문

---
+++ 노드js/nodejs/syntax/comparison.js
@@ -0,0 +1,7 @@
+// comparison -> 비교연산자
+console.log(1==1); // true
+console.log(1==2); // false
+console.log(1>2); // false
+console.log(1<2); // true
+console.log(1===1); // true (정확히 같냐를 비교할때 ===)
+console.log(1===2); // false
