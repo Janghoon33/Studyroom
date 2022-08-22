86607fc365ee06d529e16b8d165724129948a23a
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-08-22 10:23:44 +0900

    Node.js 공부 2일차 본문구현

--- 노드js/nodejs/main.js
+++ 노드js/nodejs/main.js
@@ -5,15 +5,38 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
     console.log(queryData.id);
     if(_url == '/'){
-      url = '/index.html';
+      title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    // response.end(fs.readFileSync(__dirname + url));
-    response.end(queryData.id);
+    fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="index.html">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+
+      `;
+      response.end(template);
+    })
+
 });
 app.listen(3000);
