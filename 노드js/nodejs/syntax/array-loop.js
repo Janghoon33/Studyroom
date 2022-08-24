cf956b2d71b178bc3f160b979e051535c1aabaf5
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-08-24 17:14:35 +0900

    Node.js 공부 4일차 배열

---
+++ 노드js/nodejs/syntax/array-loop.js
@@ -0,0 +1,10 @@
+// 반복문+배열
+var number = [1,40,12,34];
+var i = 0;
+var total = 0;
+while(i < number.length ){
+  total = total + number[i];
+  i = i + 1;
+
+}
+console.log(`total : ${total}`);
---
+++ 노드js/nodejs/syntax/array.js
@@ -0,0 +1,7 @@
+// 정보시스템의 핵심 : CRUD
+var arr = ['A','B','C','D']; // 배열 생성
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3; // 배열 수정
+console.log(arr);
+console.log(arr.length); // 배열의 크기
