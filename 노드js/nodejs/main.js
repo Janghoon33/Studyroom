bc7925be1b2224d0fb791d548f5e860537bbb355
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-08-29 10:34:34 +0900

    Node.js 공부 6일차 pm2활용 및 글생성 UI

--- 노드js/nodejs/main.js
+++ 노드js/nodejs/main.js
@@ -11,8 +11,9 @@ function templateHTML(title, list, body){
     <meta charset="utf-8">
   </head>
   <body>
-    <h1><a href="index.html">WEB</a></h1>
+    <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -32,7 +33,6 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
 
     if(pathname === '/'){
       if(queryData.id === undefined){
@@ -59,7 +59,26 @@ var app = http.createServer(function(request,response){
       });
     }
 
-    } else{
+  } else if(pathname === '/create'){
+    fs.readdir('./data', function(error, filelist){
+      var title = 'WEB - create';
+      var list = templateList(filelist);
+      var template = templateHTML(title,list, `
+        <form action="http://localhost:3000/process_create" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `);
+      response.writeHead(200);
+      response.end(template);
+    });
+
+  } else {
       response.writeHead(404);
       response.end('Not found')
     }
