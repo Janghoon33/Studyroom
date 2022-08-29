30e19f222e3ac66acd2d14c850441af8a4862720
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-08-29 11:37:50 +0900

    Node.js 공부 6일차 Post로 데이터 전송, 파일생성 및 리다이렉션

--- 노드js/nodejs/main.js
+++ 노드js/nodejs/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
@@ -64,7 +65,7 @@ var app = http.createServer(function(request,response){
       var title = 'WEB - create';
       var list = templateList(filelist);
       var template = templateHTML(title,list, `
-        <form action="http://localhost:3000/process_create" method="post">
+        <form action="http://localhost:3000/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -76,6 +77,21 @@ var app = http.createServer(function(request,response){
         `);
       response.writeHead(200);
       response.end(template);
+    });
+
+  } else if(pathname === '/create_process'){
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end',function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`,description, 'utf8', function(err){
+        response.writeHead(302,{Location:`/?id=${title}`}); // 200 : 성공, 302 : 페이지를 다른곳에 보낼 때
+        response.end();
+      })
     });
 
   } else {
