9e8938235ad7c123e2a2efaeee8bfbb702be1946
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2022-09-13 15:32:25 +0900

    Node.js 함수(표현/선언)

---
+++ 노드js/nodejs(스인재ver)/0913/ex01Function.js
@@ -0,0 +1,15 @@
+// 함수 : 필요할 때마다 사용하는 기능
+
+call1();
+
+function call1() {
+  console.log("call1함수호출");  
+}// 함수 선언
+
+function call1() {
+  console.log("call1함수호출-2");  
+}// 함수 선언
+
+//call2 = function(){
+
+//}// 함수 표현
