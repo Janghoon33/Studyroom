019d5d660fbff4820e68e97c907258d273ecf912
전장훈 <h970303@naver.com>
2023-10-31 00:05:36 +0900

    인텔리제이 설정 및 hello world 찍기

---
+++ 자바/SpringBoot/firstproject/src/test/java/com/jh/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
