5017b4a4b052ef7507de695d8b3172287f57817d
전장훈 <h970303@naver.com>
2023-11-07 23:22:33 +0900

    form 데이터를 이용한 controller, dto 복습

---
+++ 자바/SpringBoot/firstproject/src/main/java/com/jh/firstproject/dto/ArticleForm.java
@@ -0,0 +1,21 @@
+package com.jh.firstproject.dto;
+
+public class ArticleForm {
+
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+}
