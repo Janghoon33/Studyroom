243cf282e5b24d88e042f5412eb1a31b103427b2
전장훈 <h970303@naver.com>
2024-02-14 23:19:28 +0900

    Create Spring Boot Board Project & DB connection

---
+++ 자바/SpringBoot_Board/src/main/java/com/test/Board/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.test.Board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String main(){
+        return "Hi It's me!";
+    }
+}
