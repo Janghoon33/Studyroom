0754f673e8df2643c821c2bd607b43a57f858cbc
전장훈 <h970303@naver.com>
2024-03-14 20:08:03 +0900

    react.js useState 숫자 기록, 리스팅

--- 자바/React_SpringBoot/springBoot/src/main/reactfront/src/App.js
+++ 자바/React_SpringBoot/springBoot/src/main/reactfront/src/App.js
@@ -1,16 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 
 function App() {
 
-  let [num, setNum] = useState(1);
+  let [num, setNum] = useState(0);
   
+  const [numList, setNumList] = useState([]);
+
+  function numRecording(){
+    setNumList([...numList, num]);
+
+    setNum(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <div className='number'>{ num }</div>     
-      </header>
+      <div>현재 숫자 : {num}</div>
+      <br/>
+      <button onClick={() => {setNum(num + 1)}}>증가</button>
+      <button onClick={() => {setNum(num - 1)}}>감소</button>
+      <button onClick={numRecording}>기록</button>
+      <h2>저장된 숫자</h2>
+      <ul>
+        {numList.map((num)=>(
+          <li>{num}</li>
+        ))}
+      </ul>
     </div>
   );
 }
