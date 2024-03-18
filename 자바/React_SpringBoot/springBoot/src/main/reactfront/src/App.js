8cd7e239e0033f31f3902be5414702aa2ab4f812
전장훈 <h970303@naver.com>
2024-03-18 19:02:33 +0900

    react 숫자 기록 함수 분리

--- 자바/React_SpringBoot/springBoot/src/main/reactfront/src/App.js
+++ 자바/React_SpringBoot/springBoot/src/main/reactfront/src/App.js
@@ -1,33 +1,55 @@
 import './App.css';
 import React, { useState } from 'react';
 
-function App() {
+const RecordForm = ({numList,setNumList}) => {
 
-  let [num, setNum] = useState(0);
-  
-  const [numList, setNumList] = useState([]);
+  const [num, setNum] = useState(0);
 
-  function numRecording(){
-    setNumList([...numList, num]);
-
-    setNum(0);
+  function numRecord(){
+    setNumList([...numList,num]);
   }
 
-  return (
-    <div className="App">
-      <div>현재 숫자 : {num}</div>
-      <br/>
-      <button onClick={() => {setNum(num + 1)}}>증가</button>
-      <button onClick={() => {setNum(num - 1)}}>감소</button>
-      <button onClick={numRecording}>기록</button>
-      <h2>저장된 숫자</h2>
-      <ul>
-        {numList.map((num)=>(
+  return <div>
+    <div>현재 숫자 : {num}</div>
+    <button onClick={() => setNum(num + 1)}>숫자 증가</button>
+    <button onClick={() => setNum(num - 1)}>숫자 감소</button>
+    <button onClick={() => setNum(0)}>숫자 초기화</button>
+    <hr/>
+    <button onClick={numRecord}>숫자 기록</button>
+    <button onClick={() => setNumList([])}>기록 초기화</button>
+  </div>
+}
+
+const RecordList = ({numList}) => {
+  return ( <div>
+    <h1>기록된 숫자</h1>
+    {numList.length > 0 ? <div>
+    {
+      numList.map((num)=>(
           <li>{num}</li>
-        ))}
-      </ul>
-    </div>
+        ))
+      } 
+    </div> :
+     <div>기록 없음</div>}
+    
+  </div>
   );
+}
+
+const App = () => {
+
+  const [numList , setNumList] = useState([]);
+
+  return (
+    <div style={{margin : "0 auto", 
+                 width : "800px",
+                 textAlign : "center"}}>
+      
+    <RecordForm numList={numList} setNumList={setNumList}/>
+    <br/>
+    <RecordList numList={numList}/>
+    </div>
+  )
 }
 
 export default App;
