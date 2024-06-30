e93b920c59de174f5d60f8e3851e1ff7dc9f06e3
전장훈 <h970303@naver.com>
2024-06-30 21:08:22 +0900

    vue project 생성
    공부용

---
+++ Vue/쀼우/vuedongsan/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  Hi It's Me (여기가 메인페이지) App.vue 를 index.html에 박아넣어서 웹에 표기(그걸 main.js가 함)
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
