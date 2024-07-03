9d42caad24fee04f811d15328fecac3e21bbc991
전장훈 <h970303@naver.com>
2024-07-03 23:35:01 +0900

    Vue 이벤트 핸들러

--- Vue/쀼우/vuedongsan/src/App.vue
+++ Vue/쀼우/vuedongsan/src/App.vue
@@ -3,9 +3,10 @@
     <a v-for="(변수,b) in menus" :key="b">{{ b }}</a>
   </div>
   
-  <div v-for="상품 in products" :key="상품" >
-    <h4 :style="스타일" >{{ 상품 }}</h4>  
-    <p>60 만원</p>
+  <div v-for="a in products" :key=a>
+    <h4 :style="스타일" >{{ a }}</h4>  
+    <p>월 60 만원</p>
+    <button @:click="increase">허위매물신고</button> <span>신고수 : {{ 신고수 }}</span>
   </div>
 </template>
 
@@ -15,14 +16,18 @@ export default {
   name: 'App',
   data(){ // 데이터 보관함
     return {
-      prices : [70,80,90],
+      신고수 : 0,
       스타일 : 'color : blue',
       menus : ['Home','Shop','About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸' ],
     }
   },
+  methods: { // vue 함수 공간
+    increase(){
+      this.신고수++;
+    }
+  },
   components: {
-    
   }
 }
 </script>
