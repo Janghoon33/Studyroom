b9b38fa1f75d36084ca58c9625fb25905e44c8f6
전장훈 <h970303@naver.com>
2024-07-08 23:02:42 +0900

    vue import/export 실습

--- Vue/쀼우/vuedongsan/src/App.vue
+++ Vue/쀼우/vuedongsan/src/App.vue
@@ -11,32 +11,22 @@
   <div class="menu">
     <a v-for="a in menus" :key="a">{{ a }}</a>
   </div>
-  <div>
-    <img src="./assets/image1.png" class="room-img">
-    <h4 @click="모달창 = true">{{ products[0] }}</h4>  
-    <p>월 50 만원</p>
-    <button @:click="신고수[0]++">허위매물신고</button> <span>신고수 : {{ 신고수[0] }}</span>
-  </div>
-  <div>
-    <img src="./assets/image2.jpg" class="room-img">
-    <h4>{{ products[1] }}</h4>  
-    <p>월 60 만원</p>
-    <button @:click="신고수[1]++">허위매물신고</button> <span>신고수 : {{ 신고수[1] }}</span>
-  </div>
-  <div>
-    <img src="./assets/image3.jpg" class="room-img">
-    <h4>{{ products[2] }}</h4>  
-    <p>월 70 만원</p>
-    <button @:click="신고수[2]++">허위매물신고</button> <span>신고수 : {{ 신고수[2] }}</span>
+  <div v-for="a in 원룸들" :key="a">
+    <img :src="a.image" class="room-img">
+    <h4>{{ a.title }}</h4>  
+    <p>{{ a.price }}원</p>
   </div>
 </template>
 
 <script>
 
+import data from './assets/oneroom.js';
+
 export default {
   name: 'App',
   data(){ // 데이터 보관함 == react의 state
     return {
+      원룸들 : data,
       모달창 : false,
       신고수 : [0,0,0],
       스타일 : 'color : blue',
